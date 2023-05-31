51d340d39ceacc89b139d365f3c9e9067bbb8290
Owen Taylor <owen.taylor19@icloud.com>
2023-05-31 08:35:05 -0600

    Fixed bug where player could score in opponents bucket

--- game/driver.ml
+++ game/driver.ml
@@ -15,9 +15,6 @@ let rec filledBoardFunc (board_vals: string list) (board_spacers: string list) (
           filledBoardFunc (t2) (t1) (return_board ^ h1 ^ h2)
       |[] -> 
         return_board ^ h1
-
-let convertIntToStr x = string_of_int x
-
 let prettyPrint (x: string) =
   "\n             Player 1" ^
   "\n      1   2   3   4   5   6" ^
@@ -27,47 +24,53 @@ let prettyPrint (x: string) =
   "      6   5   4   3   2   1\n" ^
   "             Player 2\n"
 
-let rec moveMarbles value index (board: int list) = 
+let rec moveMarbles (value) (index) (board: int list) (turn: bool) = 
   if value == 0 then
     board
   else
-    if index == 14 then
-      moveMarbles (value+1) (-1) (board)
+    if (turn == true) && (index == 12) then
+      moveMarbles (value) (-1) (board) (turn)
     else
-      let boardInc = List.mapi (fun i x -> if i == index+1 then x+1 else x) board in
-      moveMarbles (value-1) (index + 1) (boardInc)
+      if (turn == false) && (index == 5) then
+        moveMarbles (value) (6) (board) (turn)
+      else 
+        if index == 14 then
+          moveMarbles (value+1) (-1) (board) (turn)
+        else
+          let boardInc = List.mapi (fun i x -> if i == index+1 then x+1 else x) board in
+          moveMarbles (value-1) (index + 1) (boardInc) (turn)
 
 
 
 let rec game_func (gameBoard: int list)(t: bool) = 
   match gameBoard with
     | 0::0::0::0::0::0::_ -> 
-      let strBoard = List.map convertIntToStr gameBoard in 
+      let strBoard = List.map string_of_int gameBoard in 
       prettyPrint (filledBoardFunc (strBoard) (board_spacers) ("") ^ "\n" )
     | _::_::_::_::_::_::_::0::0::0::0::0::0::_ -> 
-      let strBoard = List.map convertIntToStr gameBoard in 
+      let strBoard = List.map string_of_int gameBoard in 
       prettyPrint (filledBoardFunc (strBoard) (board_spacers) ("") ^ "\n" )
     | _ ->
       if t == true then
         (*player 1 turn*)
-        let strBoard = List.map convertIntToStr gameBoard in
+        let strBoard = List.map string_of_int gameBoard in
         let () = print_string(prettyPrint (filledBoardFunc (strBoard) (board_spacers) ("") ^ "\n" )) in
-        let () = print_string("Player 1, Select the marbles you would like to move (1-6): ") in
+        let () = print_string("Player 1, Select the marbles you would like to move (1--6): ") in
         let cellPicked = read_int() in
-        let newBoard = moveMarbles (List.nth gameBoard (cellPicked - 1))(cellPicked - 1)(List.mapi (fun i x -> if i == cellPicked -1 then 0 else x) gameBoard) in
-        if List.nth gameBoard (cellPicked-1) + cellPicked-1 == 6 then
+        let newBoard = moveMarbles (List.nth gameBoard (cellPicked - 1))(cellPicked - 1)(List.mapi (fun i x -> if i == cellPicked -1 then 0 else x) gameBoard)(t) in
+        if (List.nth gameBoard (cellPicked-1)) + cellPicked-1 == 6 then
           let () = print_string("Player 1 goes again!") in
           game_func newBoard t
         else
           game_func newBoard (not t)
       else
         (*player 2 turn*)
-        let strBoard = List.map convertIntToStr gameBoard in 
+        let strBoard = List.map string_of_int gameBoard in 
         let () = print_string(prettyPrint (filledBoardFunc (strBoard) (board_spacers) ("") ^ "\n" )) in
         let () = print_string("Player 2, Select the marbles you would like to move (1-6): ") in
         let cellPicked = read_int() in
-        let newBoard = moveMarbles (List.nth gameBoard (cellPicked + 6))(cellPicked + 6)(List.mapi (fun i x -> if i == cellPicked+6 then 0 else x) gameBoard) in
-        if List.nth gameBoard (cellPicked+6) + cellPicked+6 == 13 then
+        let newBoard = moveMarbles (List.nth gameBoard (cellPicked + 6))(cellPicked + 6)(List.mapi (fun i x -> if i == cellPicked+6 then 0 else x) gameBoard)(t) in
+        if (List.nth gameBoard (cellPicked+6)) + cellPicked+6 == 13 then
           let () = print_string("Player 2 goes again!") in
           game_func newBoard t
         else
